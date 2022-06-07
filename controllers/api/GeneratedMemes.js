011bb4f85bb7a64408aa81ce956fea0f3709a4bf
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2022-06-07 14:03:19 +0300

    Get all generated memes + Delete the memes from DB and PF + Query Params Getter

--- controllers/api/GeneratedMemes.js
+++ controllers/api/GeneratedMemes.js
@@ -1,12 +1,51 @@
 const Meme = require("../../models/generatedMeme");
-async function show(req, res) {
+const fs = require("fs")
+
+async function showMemes(req, res) {
+    const memes = await getMemes();
+    const response = new Array();
+    let offset = req.params.offset == null ? 0 : parseInt(req.params.offset);
+    let limit = req.params.limit == null ?  memes.length - 1 : parseInt(req.params.limit)
+    
+    if (memes.length === 0) {
+            res.status(404).json({ message: "The memes table is empty!" })
+            return;
+    }
+
+    for (let i = offset; i <= limit; ++i) {
+            response.push(memes.at(i));
+    }
+    res.status(200).json(response)
+}
+async function deleteFiles(res, req) {
+    const memes = await getMemes();
+    memes.forEach(meme => {
+        const path = `./public/images/generated/${meme.file}.jpg`
+        try {
+            fs.unlinkSync(path)
+        } catch (e) {
+            console.error(e)
+        }
+    })
+    await deleteMemes(res, req)
+}
+async function deleteMemes(res, req) {
     try {
-        const memes = await Meme.find()
-        return memes
+        await Meme.deleteMany();
+        res.json("All memes are deleted")
+    } catch (e) {
+        console.error(e)
+    }
+}
+async function getMemes() {
+    try { 
+        return await Meme.find()
     } catch (err) {
-        res.status(400).json({ message: err.message })
+        console.error(err)
     }
 }
 module.exports = {
-    show
+    showMemes,
+    deleteFiles,
+    deleteMemes
 }
--- routes/api/memes.js
+++ routes/api/memes.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const defaultMemes = require("../../controllers/api/DefaultMemes");
 const post = require("../../controllers/api/Post");
-
+const generatedMemes = require("../../controllers/api/GeneratedMemes")
 // GET Default Memes
-router.get('/getDefaultMemes', defaultMemes.show);
+router.get('/getDefaultMemes', defaultMemes.showMemes);
 // POST The Meme
 router.post("/generateMemes", post.meme)
 // GET Generated Memes
-router.get('/getGeneratedMemes', async (req, res) => {
-    // From this path I will retrieve the generated memes from Database
-})
+router.get('/getGeneratedMemes', generatedMemes.showMemes)
+router.get('/getGeneratedMemes/:offset-:limit', generatedMemes.showMemes)
+// DELETE All Generated Memes from DB
+router.delete("/deleteGeneratedMemes", generatedMemes.deleteFiles)
 
 module.exports = router
