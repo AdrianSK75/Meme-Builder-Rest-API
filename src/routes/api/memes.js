2aca6e225fa9b6946c3d52d238878f6013458762
AdrianSK <56803219+AdrianSK75@users.noreply.github.com>
2022-06-18 12:37:41 +0300

    Create File Text, Test

--- src/routes/api/memes.js
+++ src/routes/api/memes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs")
 const router = express.Router();
 const defaultMemes = require("../../controllers/api/DefaultMemes");
 const post = require("../../controllers/api/Post");
@@ -11,5 +12,11 @@ router.post("/createMeme", post.createMeme)
 router.get('/getGeneratedMemes', generatedMemes.getMemes)
 // DELETE All Generated Memes from DB
 router.delete("/deleteGeneratedMemes", generatedMemes.deleteFiles)
+
+router.get("/create-txt-file", (req, res, next) => {
+    const text = req.query.txt;
+    fs.writeFileSync(`./public/text/${text}.txt`, text)
+    res.status(201).json("File created!")
+})
 
 module.exports = router
